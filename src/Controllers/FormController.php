2549e19a144003fc992a2c83464e4eeb3980e3e0
garretsn <garretsn@gmail.com>
2022-02-12 15:09:09 +0200

    Update FormController.php

--- src/Controllers/FormController.php
+++ src/Controllers/FormController.php
@@ -6,7 +6,7 @@ class FormController
 {
     public function formData($data)
     {
-        if(!$this->validation($data)) {
+        if ( ! $this->validation($data)) {
             print_r('nevalidat');
         }
 
@@ -14,7 +14,44 @@ class FormController
 
     private function validation($data)
     {
-        print_r($data);
+//        $firstname = $this->cleanInput($data['firstname']);
+//        $lastname = $this->cleanInput($data['lastname']);
+//        $companyname = $this->cleanInput($data['companyname']);
+//        $address1 = $this->cleanInput($data['address1']);
+//        $address2 = $this->cleanInput($data['address2']);
+//        $city = $this->cleanInput($data['city']);
+//        $county = $this->cleanInput($data['county']);
+//        $zipcode = $this->cleanInput($data['zipcode']);
+//        $country = $this->cleanInput($data['country']);
+//        $phonenumber = $this->cleanInput($data['phonenumber']);
+//        $email = $this->cleanInput($data['email']);
+        $formData = $data;
+        array_walk(
+            $formData,
+            function (&$key, $value) {
+                $value = $this->cleanInput($value);
+            }
+        );
+        print_r($formData);
+
+        return true;
+    }
+
+    private function cleanInput($input)
+    {
+        $input = trim($input);
+        $input = stripslashes($input);
+        $input = htmlspecialchars($input);
+
+        return $input.'iepuri';
+    }
+
+    private function validateStringLength(string $string, int $length)
+    {
+        if (strlen($string) > $length) {
+            return false;
+        }
+
         return true;
     }
 }
