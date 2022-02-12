1991440f7487c5477a88abe91a7ea0346ee170e6
garretsn <garretsn@gmail.com>
2022-02-12 13:31:15 +0200

    form ready

--- src/Renders/form.php
+++ src/Renders/form.php
@@ -8,11 +8,47 @@
     <title>Label Generator Form</title>
 </head>
 <body>
-<form>
-    <label for="fname">First name:</label><br>
-    <input type="text" id="fname" name="fname"><br>
-    <label for="lname">Last name:</label><br>
-    <input type="text" id="lname" name="lname">
+<form action="form.php" method="post">
+    <label for="firstname">First name:</label><br>
+    <input type="text" id="firstname" name="firstname"><br>
+
+    <label for="lastname">Last name:</label><br>
+    <input type="text" id="lastname" name="lastname">
+
+    <label for="companyname">Company name:</label><br>
+    <input type="text" id="companyname" name="companyname">
+
+    <label for="address1">Street Address 1:</label><br>
+    <input type="text" id="address1" name="address1">
+
+    <label for="address2">Street Address 2:</label><br>
+    <input type="text" id="address2" name="address2">
+
+    <label for="city">City:</label><br>
+    <input type="text" id="city" name="city">
+
+    <label for="county">County/State:</label><br>
+    <input type="text" id="county" name="county">
+
+    <label for="zipcode">Zip Code:</label><br>
+    <input type="text" id="zipcode" name="zipcode">
+
+    <label for="country">Country:</label><br>
+    <input type="text" id="country" name="country">
+
+    <label for="phonenumber">Phone Number:</label><br>
+    <input type="text" id="phonenumber" name="phonenumber">
+
+    <label for="email">Email:</label><br>
+    <input type="text" id="email" name="email">
+
+    <input type="submit" value="Submit">
 </form>
 </body>
 </html>
+
+<?php
+if (isset($_POST[ 'submit' ])) {
+    print_r($_POST);;
+}
+?>
