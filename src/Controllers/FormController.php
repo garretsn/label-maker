330677162471b12e2436ad0a63fe5ed0438fd30f
garretsn <garretsn@gmail.com>
2022-02-12 14:27:09 +0200

    Update FormController.php

--- src/Controllers/FormController.php
+++ src/Controllers/FormController.php
@@ -4,14 +4,9 @@ namespace Garret\LabelMaker\Controllers;
 
 class FormController
 {
-    public function render()
+    public function formData($data)
     {
-//        ob_start();
-//        include('form.php');
-//        $applied_template = ob_get_contents();
-//        ob_end_clean();
-//
-//        echo $applied_template;
-        print_r('iepuri');
+        print_r($data);
+
     }
 }
