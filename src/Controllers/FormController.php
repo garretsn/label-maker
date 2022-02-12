c002668665f0c3e1d5a5da7e1eed9d5cda86d856
garretsn <garretsn@gmail.com>
2022-02-12 14:04:09 +0200

    refact

---
+++ src/Controllers/FormController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Garret\LabelMaker\Controllers;
+
+class FormController
+{
+    public function render()
+    {
+//        ob_start();
+//        include('form.php');
+//        $applied_template = ob_get_contents();
+//        ob_end_clean();
+//
+//        echo $applied_template;
+        print_r('iepuri');
+    }
+}
