b89db99b1206b5c7341ca9737b26f845728cdad2
garretsn <garretsn@gmail.com>
2022-02-12 16:49:43 +0200

    revert

--- src/Controllers/FormController.php
+++ src/Controllers/FormController.php
@@ -13,39 +13,20 @@ class FormController
         $this->formValidator = $formValidator;
     }
 
-    public function generateLabel($data) //make this generateLabel
+    public function formData($data) //make this generateLabel
     {
         if ( !$this->formValidator->validation($data)) {
             print_r('Not valid input, cannot generate label');
             return false;
         }
-//        print_r($this->formValidator->validation($data));
-        $this->render('label.php', $this->formValidator->validation($data));
+        print_r($this->formValidator->validation($data));
         return true;
     }
 
-    public function function_get_output($fn)
-    {
-        $args = func_get_args();unset($args[0]);
-        ob_start();
-        call_user_func_array($fn, $args);
-        $output = ob_get_contents();
-        ob_end_clean();
-        return $output;
+    private function generateHash ($firstname, $lastname, $zipcode, $phonenumber, $email) {
+        $largeString = $firstname.$lastname.$zipcode.$phonenumber.$email;
+        return hash('md5', $largeString);
     }
-
-    public function display($template, $params = array())
-    {
-        extract($params);
-        include $template;
-    }
-
-    public function render($template, $params = array())
-    {
-        return $this->function_get_output('display', $template, $params);
-    }
-
-
 
 
 }
