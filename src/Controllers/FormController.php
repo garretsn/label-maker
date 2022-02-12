1c71a683cecf6dfec4eac794d1f50cc2a3ebb9be
garretsn <garretsn@gmail.com>
2022-02-12 16:46:30 +0200

    test render

--- src/Controllers/FormController.php
+++ src/Controllers/FormController.php
@@ -19,8 +19,30 @@ class FormController
             print_r('Not valid input, cannot generate label');
             return false;
         }
-        print_r($this->formValidator->validation($data));
+//        print_r($this->formValidator->validation($data));
+        $this->render('label.php', $this->formValidator->validation($data));
         return true;
+    }
+
+    public function function_get_output($fn)
+    {
+        $args = func_get_args();unset($args[0]);
+        ob_start();
+        call_user_func_array($fn, $args);
+        $output = ob_get_contents();
+        ob_end_clean();
+        return $output;
+    }
+
+    public function display($template, $params = array())
+    {
+        extract($params);
+        include $template;
+    }
+
+    public function render($template, $params = array())
+    {
+        return $this->function_get_output('display', $template, $params);
     }
 
 
